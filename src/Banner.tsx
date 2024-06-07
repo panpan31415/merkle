0a985291c8c697fe3390aacd2d4a90f867535920
panpan <panpan31415@gmail.com>
2024-06-07 08:58:35 +0200

    rename class name

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -4,19 +4,21 @@ export default function Banner() {
         <section className="banner">
             <div className="banner__container">
                 <div className="banner__left">
-                    <div className="text-container">
-                        <a href="https//www.panpan.dk" className="text-container__author-link" rel="noreferrer" target="_blank">By Panpan Zhang </a>
-                        <div className="text-container__title-container">
-                            <h1 className="text-container__slogan-title">Here’s All the Hack News You already Missed</h1>
+                    <div className="slogan-container">
+                        <a href="https//www.panpan.dk" className="slogan-container__author-link" rel="noreferrer" target="_blank">By Panpan Zhang </a>
+                        <div className="slogan-container__title-container">
+                            <h1 className="slogan-container__title">Here’s All the Hack News You already Missed</h1>
                         </div>
-                        <p className="text-container__slogan-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
+                        <p className="slogan-container__description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
                             qui maio ionem quis menda  nemo
                             ?</p>
-                        <button className="text-container__button">Read More</button>
+                        <button className="slogan-container__button">Read More</button>
                     </div>
 
                 </div>
-                <div className="banner__right"></div>
+                <div className="banner__right">
+
+                </div>
             </div>
             <div className="banner__divider">1</div>
         </section>
